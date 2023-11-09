03ce3e1c971d08dd43a247454cf27d7618c8003d
gsy0911 <yoshiki0911@gmail.com>
2023-11-09 22:09:44 +0900

    remain

--- README.md
+++ README.md
@@ -2,3 +2,86 @@
 
 [![cdk-version](https://img.shields.io/badge/aws_cdk-2.70.0-green.svg)](https://formulae.brew.sh/formula/aws-cdk)
 [![NodeVersion](https://img.shields.io/badge/node-18.15.0-blue.svg)](https://nodejs.org/ja/)
+
+# (optional) prepare local machine
+
+install `Session Manager plugin` from [here](https://docs.aws.amazon.com/ja_jp/systems-manager/latest/userguide/session-manager-working-with-install-plugin.html).
+
+# deploy step
+
+## 1. create `infrastructure/params` from example-file
+
+```shell
+$ cd infrastructure
+$ cp params.example.ts params.ts
+```
+
+## 2. edit `params.ts`
+
+- before
+
+```typescript
+import {IEc2Ssm} from './Ec2SsmStack';
+import {
+  Environment
+} from 'aws-cdk-lib';
+
+export const env: Environment = {
+  account: "000011112222",
+  region: "ap-northeast-1"
+}
+
+export const paramsEc2Ssm: IEc2Ssm = {
+  applicationSuffix: "zenn-example",
+  vpcId: "vpc-{your-vpc-id}",
+}
+```
+
+- after
+```typescript
+import {IEc2Ssm} from './Ec2SsmStack';
+import {
+  Environment
+} from 'aws-cdk-lib';
+
+export const env: Environment = {
+  account: "333344445555",
+  region: "ap-northeast-1"
+}
+
+export const paramsEc2Ssm: IEc2Ssm = {
+  applicationSuffix: "zenn-example",
+  vpcId: "vpc-edited",
+}
+```
+
+## 3. deploy
+
+```shell
+$ cdk deploy
+```
+
+# usage
+
+Login EC2
+
+```shell
+# after deploy
+$ EC2_INSTANCE_ID={...}
+$ aws ssm start-session --target $EC2_INSTANCE_ID
+```
+
+Login as `ec2-user`
+
+```shell
+# shell at EC2
+sh-4.2$ sudo su --login ec2-user
+
+[ec2-user@ip-0-0-0-0 ~]$ whoami
+ec2-user
+```
+
+# References
+
+- [Amazon EC2でCentOS 6.4のAMIを自前で作る方法](https://www.ryuzee.com/contents/blog/6513)
+- [EC2にEBSボリュームを新しくアタッチする](https://dev.classmethod.jp/articles/attach-ebs-to-ec2/)
--- infrastructure/lib/Ec2SsmStack.ts
+++ infrastructure/lib/Ec2SsmStack.ts
@@ -34,7 +34,8 @@ export class Ec2SsmStack extends Stack {
       managedPolicies: [
         aws_iam.ManagedPolicy.fromManagedPolicyArn(this, "AmazonEC2ContainerServiceforEC2Role", "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"),
         /** Add managed policy to use SSM */
-        aws_iam.ManagedPolicy.fromManagedPolicyArn(this, "AmazonEC2RoleforSSM", "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM")
+        aws_iam.ManagedPolicy.fromManagedPolicyArn(this, "AmazonEC2RoleforSSM", "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"),
+        aws_iam.ManagedPolicy.fromManagedPolicyArn(this, "S3FullAccess", "arn:aws:iam::aws:policy/AmazonS3FullAccess"),
       ]
     })
     new aws_iam.CfnInstanceProfile(this, `instance-profile`, {
@@ -43,15 +44,29 @@ export class Ec2SsmStack extends Stack {
     })
 
     /** 実行環境としてのEC2 */
-    new aws_ec2.BastionHostLinux(this, 'BastionHost', {
-      vpc,
+    // const machineImage = aws_ec2.MachineImage.latestAmazonLinux({
+    //   generation: aws_ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+    //   edition: aws_ec2.AmazonLinuxEdition.STANDARD,
+    //   kernel: aws_ec2.AmazonLinuxKernel.KERNEL5_X,
+    //   virtualization: aws_ec2.AmazonLinuxVirt.HVM,
+    //   storage: aws_ec2.AmazonLinuxStorage.GENERAL_PURPOSE,
+    //   cpuType: aws_ec2.AmazonLinuxCpuType.X86_64,
+    // })
+    new aws_ec2.Instance(this, "ssm-instance", {
+      instanceType: new aws_ec2.InstanceType("t3.micro"),
+      instanceName: `ssm-example-${params.applicationSuffix}`,
+      vpc: vpc,
+      machineImage: aws_ec2.MachineImage.latestAmazonLinux(),
+      role,
+      securityGroup,
       blockDevices: [{
         deviceName: '/dev/sda1',
         volume: aws_ec2.BlockDeviceVolume.ebs(10, {
           encrypted: true,
         }),
       }],
-    });
+      keyName: "arzon-centos"
+    })
 
   }
 }
